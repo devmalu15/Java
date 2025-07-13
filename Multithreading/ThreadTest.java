8fedc2f89363de98a9615c1c3f5ad3764fe3a055
Dev Malu <maludev15@gmail.com>
2025-07-13 15:43:45 +0530

    .Add Multithreading Folder

---
+++ Multithreading/ThreadTest.java
@@ -0,0 +1,41 @@
+package Multithreading;
+
+public class ThreadTest {
+	
+	public static void main(String[] args) {
+		new A().start();
+		new B().start();
+		new C().start();
+	}
+
+}
+
+class A extends Thread{
+	public void run() {
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("\t From thread A : i = " + i);
+		}
+		System.out.println("Exit from A");
+	}
+}
+
+
+class B extends Thread{
+	public void run() {
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("\t From thread B : i = " + i);
+		}
+		System.out.println("Exit from B");
+	}
+}
+
+class C extends Thread{
+	public void run() {
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("\t From thread C : i = " + i);
+		}
+		System.out.println("Exit from C");
+	}
+}
+
+
